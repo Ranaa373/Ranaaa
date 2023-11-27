20309f08e5f3960be035006327fded8480eabb4a
Ranaa373 <152155243+Ranaa373@users.noreply.github.com>
2023-11-27 16:49:09 +0800

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,5 @@
+// FUNGSI PADA TOMBOL BAWAH KANAN UNTUK MENAMPILKAN ALERT
+const alertButton = document.getElementById("button");
+alertButton.addEventListener('click', function() {
+alert('Kirim ke whatsapp admin untuk menanyakan sesuatu');
+});
